77ffc2e07aa5dd9a73f32563695d27abf09f0e96
angelitasantos <angelitasantos@gmail.com>
2021-02-10 18:50:46 -0300

    created new fields table: users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fullname, presence: true, length: { maximum: 70 }
+  validates :organization, presence: false, length: { maximum: 150 }
 end
---
+++ db/migrate/20210210202413_add_extra_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddExtraFieldsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :fullname, :string
+    add_column :users, :organization, :string
+    add_column :users, :phone, :string
+    add_column :users, :about, :text
+    add_column :users, :status, :boolean, default: false
+  end
+end
